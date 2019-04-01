0109499383f6b637d5368b8e416c53fb9b9a2ba3
michelle-pasquill <mimi.191234@hotmail.com>
2019-04-01 18:15:36 -0400

    Added the window.onload.

--- game/js/game.js
+++ game/js/game.js
@@ -1,174 +1,175 @@
 window.onload = function() {
   loadCanvas();
-};
 
-var stage;
+  var stage;
 
-var background;
-var bounds;
-var currSprites = {};
+  var background;
+  var bounds;
+  var currSprites = {};
 
-var queue;
-var loadBar;
+  var queue;
+  var loadBar;
 
-var prevScene = 0;
-var nextScene = 1;
+  var prevScene = 0;
+  var nextScene = 1;
 
-var unvisited = ["Beach", "Desert", "Mountains", "Waterfall", "Forest"];
+  var unvisited = ["Beach", "Desert", "Mountains", "Waterfall", "Forest"];
 
-// Page button listeners
-function switchPage(type) {
-  var scene;
-  if (type == "prev") {
-    scene = prevScene;
-  } else {
-    scene = nextScene;
+  // Page button listeners
+  function switchPage(type) {
+    var scene;
+    if (type == "prev") {
+      scene = prevScene;
+    } else {
+      scene = nextScene;
+    }
+
+    if (scene == 0) {
+      resetSprites();
+      scene0();
+    } else if (scene == 1) {
+      scene1();
+    } else if (scene == 2) {
+      scene2();
+    }
+
+    // Update prev and next
+    if (scene == 0) {
+      prevScene = 0;
+      nextScene = 1;
+    } else {
+      prevScene = scene - 1;
+      nextScene = scene + 1;
+    }
   }
 
-  if (scene == 0) {
-    resetSprites();
+  $("#prev").click(function() {
+    switchPage("prev");
+  });
+
+  $("#next").click(function() {
+    console.log("here");
+    switchPage("next");
+  });
+
+  // Createjs
+  function loadCanvas() {
+    stage = new createjs.Stage("gameCanvas");
+
+    loadBar = new createjs.Shape();
+    loadBar.graphics.beginFill("blue").drawRect(0, 165, 1, 20).endFill();
+
+    stage.addChild(loadBar);
+
+    queue = new createjs.LoadQueue();
+    queue.loadManifest([
+      {id: "planetBG", src: "assets/planet.jpg"},
+      {id: "spaceBG", src: "assets/space.jpg"},
+      {id: "beachBG", src: "assets/Beach/game_background_1.png"},
+      {id: "desertBG", src: "assets/Desert/background1.png"},
+      {id: "mountainBG", src: "assets/Earth/game_background_1.png"},
+      {id: "waterfallBG", src: "assets/Earth/game_background_4.png"},
+      {id: "forestBG", src: "assets/Earth/game_background_3.1.png"},
+      {id: "alienTurn", src: "assets/Aliens/alienBlue.png"},
+      {id: "alienStand", src: "assets/Aliens/alienBlue_stand.png"},
+      {id: "p0", src: "assets/Aliens/alienBeige_stand.png"},
+      {id: "p1", src: "assets/Aliens/alienPink_stand.png"},
+      {id: "ship", src: "assets/ufo.png"},
+      {id: "earth", src: "assets/earth-globe.png"}
+    ]);
+
+    queue.on("progress", setBar);
+    queue.on("complete", start, this);
+
+    stage.update();
+  }
+
+  function start() {
+    // Remove load bar.
+    stage.removeChild(loadBar);
+
+    // Load first scene
     scene0();
-  } else if (scene == 1) {
-    scene1();
-  } else if (scene == 2) {
-    scene2();
   }
 
-  // Update prev and next
-  if (scene == 0) {
-    prevScene = 0;
-    nextScene = 1;
-  } else {
-    prevScene = scene - 1;
-    nextScene = scene + 1;
+  function setBar(e) {
+    loadBar.scaleX = e.progress * 990;
+    stage.update();
   }
-}
 
-document.getElementById("prev").addEventListener("click", function() {
-  switchPage("prev");
-});
-
-document.getElementById("next").addEventListener("click", function() {
-  console.log("here");
-  switchPage("next");
-});
-
-// Createjs
-function loadCanvas() {
-  stage = new createjs.Stage("gameCanvas");
-
-  loadBar = new createjs.Shape();
-  loadBar.graphics.beginFill("blue").drawRect(0, 265, 1, 20).endFill();
-
-  stage.addChild(loadBar);
-
-  queue = new createjs.LoadQueue();
-  queue.loadManifest([
-    {id: "planetBG", src: "assets/planet.jpg"},
-    {id: "spaceBG", src: "assets/space.jpg"},
-    {id: "beachBG", src: "assets/Beach/game_background_1.png"},
-    {id: "desertBG", src: "assets/Desert/background1.png"},
-    {id: "mountainBG", src: "assets/Earth/game_background_1.png"},
-    {id: "waterfallBG", src: "assets/Earth/game_background_4.png"},
-    {id: "forestBG", src: "assets/Earth/game_background_3.1.png"},
-    {id: "alienTurn", src: "assets/Aliens/alienBlue.png"},
-    {id: "alienStand", src: "assets/Aliens/alienBlue_stand.png"},
-    {id: "p0", src: "assets/Aliens/alienBeige_stand.png"},
-    {id: "p1", src: "assets/Aliens/alienPink_stand.png"},
-    {id: "ship", src: "assets/ufo.png"},
-    {id: "earth", src: "assets/earth-globe.png"}
-  ]);
-
-  queue.on("progress", setBar);
-  queue.on("complete", start, this);
-
-  stage.update();
-}
-
-function start() {
-  // Remove load bar.
-  stage.removeChild(loadBar);
-
-  // Load first scene
-  scene0();
-}
-
-function setBar(e) {
-  loadBar.scaleX = e.progress * 990;
-  stage.update();
-}
-
-function loadBackground(bg) {
-  // remove current background if there is one
-  if (background != null) {
-    stage.removeChild(background);
-    background = null;
+  function loadBackground(bg) {
+    // remove current background if there is one
+    if (background != null) {
+      stage.removeChild(background);
+      background = null;
+    }
+    // Add new background
+    background = new createjs.Bitmap(bg);
+    stage.addChild(background);
+    stage.update();
+    bounds = background.getBounds();
   }
-  // Add new background
-  background = new createjs.Bitmap(bg);
-  stage.addChild(background);
-  stage.update();
-  bounds = background.getBounds();
-}
 
-function addSprite(id) {
-  var img = queue.getResult(id);
-  var bm = new createjs.Bitmap(img);
-  currSprites[id] = bm;
-  stage.addChild(bm);
-  return bm;
-}
-
-function resetSprites() {
-  for(var key in currSprites) {
-    stage.removeChild(currSprites[key]);
+  function addSprite(id) {
+    var img = queue.getResult(id);
+    var bm = new createjs.Bitmap(img);
+    currSprites[id] = bm;
+    stage.addChild(bm);
+    return bm;
   }
-  currSprites = {};
-}
 
-function scene0() {
-  // Get planet Image
-  var bg = queue.getResult("planetBG");
-  loadBackground(bg);
+  function resetSprites() {
+    for(var key in currSprites) {
+      stage.removeChild(currSprites[key]);
+    }
+    currSprites = {};
+  }
 
-  // Put alien sprite
-  var alien = addSprite("alienTurn");
-  alien.x = bounds.width / 2;
-  alien.y = bounds.height - 150;
+  function scene0() {
+    // Get planet Image
+    var bg = queue.getResult("planetBG");
+    loadBackground(bg);
 
-  stage.update();
-}
+    // Put alien sprite
+    var alien = addSprite("alienTurn");
+    alien.x = bounds.width / 2;
+    alien.y = bounds.height - 150;
 
-function scene1() {
-  console.log("scene1");
+    stage.update();
+  }
 
-  resetSprites();
+  function scene1() {
+    console.log("scene1");
 
-  // Add alien
-  var alien = addSprite("alienStand");
-  alien.x = 200;
-  alien.y = bounds.height - 150;
+    resetSprites();
 
-  // Add parents
-  var p0 = addSprite("p0");
-  p0.x = bounds.width - 200;
-  p0.y = bounds.height - 150;
+    // Add alien
+    var alien = addSprite("alienStand");
+    alien.x = 200;
+    alien.y = bounds.height - 150;
 
-  var p1 = addSprite("p1");
-  p1.x = bounds.width - 300;
-  p1.y = bounds.height - 150;
+    // Add parents
+    var p0 = addSprite("p0");
+    p0.x = bounds.width - 200;
+    p0.y = bounds.height - 150;
 
-  stage.update();
+    var p1 = addSprite("p1");
+    p1.x = bounds.width - 300;
+    p1.y = bounds.height - 150;
 
-}
+    stage.update();
 
-function scene2() {
+  }
 
-  resetSprites();
+  function scene2() {
 
-  var bg = queue.getResult("spaceBG");
-  loadBackground(bg);
+    resetSprites();
 
-  stage.update();
+    var bg = queue.getResult("spaceBG");
+    loadBackground(bg);
 
-}
+    stage.update();
+
+  }
+
+};
